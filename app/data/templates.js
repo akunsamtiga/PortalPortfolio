893b4a5c5b5629953043ed338e7799108d0c41ff
akunsamtiga <akunsamtiga@gmail.com>
2025-06-15 10:05:18 +0700

    f

--- app/data/templates.js
+++ app/data/templates.js
@@ -3,19 +3,19 @@ const templates = [
     id: 'clean-resume',
     title: 'Noelle',
     image: '/templates/p1.png',
-    url: 'https://your-demo-link.com/clean-resume',
+    url: 'https://portfolio1-vert-ten.vercel.app/',
   },
   {
     id: 'creative-grid',
     title: 'Carlos',
     image: '/templates/p2.png',
-    url: 'https://your-demo-link.com/creative-grid',
+    url: 'https://portfolio2-alpha-olive-15.vercel.app/',
   },
   {
     id: 'modern-personal',
     title: 'Sorelle',
     image: '/templates/p3.png',
-    url: 'https://your-demo-link.com/modern-personal',
+    url: 'https://portfolio3-eeps.vercel.app/',
   },
 ];
 
